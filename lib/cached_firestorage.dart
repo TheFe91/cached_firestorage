e64df9af7e1e6ce6c16c0774ca994fbe15d885f8
Alessandro Defendenti <alessandro.defendenti@radicalbit.io>
2022-05-19 12:24:44 +0200

    updated doc

--- lib/cached_firestorage.dart
+++ lib/cached_firestorage.dart
@@ -1,8 +1,10 @@
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:get_storage/get_storage.dart';
 
+///
 /// A Dart utility which helps you manage the communication between Firebase Storage and you app.
 /// It natively implements a low dependencies cache to save time and computational costs.
+///
 class CachedFirestorage {
   static CachedFirestorage? _instance;
   final Map<String, String> _storageKeys = {'default': 'default'};
@@ -10,11 +12,12 @@ class CachedFirestorage {
 
   CachedFirestorage._(this.cacheTimeout);
 
+  /// Get the singleton instance
   static CachedFirestorage get instance =>
       _instance ??= CachedFirestorage._(360);
 
-  Future<String> _getDownloadURL(
-    String filePath, {
+  /// Private method
+  Future<String> _getDownloadURL(String filePath, {
     String? fallbackFilePath,
   }) async {
     try {
@@ -30,6 +33,7 @@ class CachedFirestorage {
     }
   }
 
+  /// Gets Firebase Storage download URL and stores it into a cache
   Future<String> getDownloadURL({
     required String mapKey,
     required String filePath,
@@ -44,8 +48,10 @@ class CachedFirestorage {
 
     if (mapDownloadURLs[mapKey] != null) {
       final DateTime lastWrite =
-          DateTime.parse(mapDownloadURLs[mapKey]['lastWrite']);
-      final int difference = now.difference(lastWrite).inMinutes;
+      DateTime.parse(mapDownloadURLs[mapKey]['lastWrite']);
+      final int difference = now
+          .difference(lastWrite)
+          .inMinutes;
       if (difference < cacheTimeout) {
         return mapDownloadURLs[mapKey]['value'];
       }
@@ -66,6 +72,7 @@ class CachedFirestorage {
     return downloadURL;
   }
 
+  /// Deletes a cache entry
   void removeCacheEntry({
     required String mapKey,
     String? storageKey,
@@ -79,7 +86,4 @@ class CachedFirestorage {
           .write(_storageKeys[storageKey ?? 'default']!, mapDownloadURLs);
     }
   }
-
-  Map<String, dynamic> getMapDownloadURLs({String? storageKey}) =>
-      GetStorage().read(_storageKeys[storageKey ?? 'default']!) ?? {};
 }
